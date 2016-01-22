6734ff95c53499917ed1d79071eaa396dc5b3606
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-01-22 14:47:05 +0100

    [FIX] Fixed bad module loaded

--- __init__.py
+++ __init__.py
@@ -22,7 +22,6 @@
 #
 ##############################################################################
 
-
-import product
+import models
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
